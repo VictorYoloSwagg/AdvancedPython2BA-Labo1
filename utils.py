ac9646b6c2f9a0dc6495e939ad926f61c12a7f13
VictorYoloSwagg <victor.pramann@gmail.com>
2022-05-03 14:32:00 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,59 @@
+def fact(n):
+	"""Computes the factorial of a natural number.
+	
+	Pre: -
+	Post: Returns the factorial of 'n'.
+	Throws: ValueError if n < 0
+	"""
+	pass
+
+def roots(a, b, c):
+	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
+	
+	Pre: -
+	Post: Returns a tuple with zero, one or two elements corresponding
+		to the roots of the ax^2 + bx + c polynomial.
+	"""
+	pass
+
+def integrate(function, lower, upper):
+	"""Approximates the integral of a fonction between two bounds
+	
+	Pre: 'function' is a valid Python expression with x as a variable,
+		'lower' <= 'upper',
+		'function' continuous and integrable between 'lower‘ and 'upper'.
+	Post: Returns an approximation of the integral from 'lower' to 'upper'
+		of the specified 'function'.
+
+	Hint: You can use the 'integrate' function of the module 'scipy' and
+		you'll probably need the 'eval' function to evaluate the function
+		to integrate given as a string.
+	"""
+	pass
+
+if __name__ == '__main__':
+	print(fact(5))
+	print(roots(1, 0, 1))
+	print(integrate('x ** 2 - 1', -1, 1))
+
+from git import Repo
+import os
+
+path = 'C:\Users\user\Downloads\utils.py'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+os.chdir(path)
+
+repo = Repo.init(path).git
+
+index = Repo.init(path).index
+
+for x in xrange (1,10):
+    fname = 'filename' + str(x)
+    f.open(fname, 'wb+')
+    f.write()
+    f.close()
+    repo.add(fname)
+
+index.commit("initial commit")
